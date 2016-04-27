b9a84993f3d53b8eb49c654b2bbf63f60bfc2128
Fabian Pedregosa <f@bianp.net>
2016-04-27 13:32:48 +0200

    more

--- hoag/multilogistic.py
+++ hoag/multilogistic.py
@@ -81,6 +81,83 @@ class MultiLogisticRegressionCV(linear_model.base.BaseEstimator,
     def predict(self, X):
         return np.sign(self.decision_function(X))
 
+
+
+class MultiLogisticRegression(linear_model.base.BaseEstimator,
+                           linear_model.base.LinearClassifierMixin):
+
+    def __init__(self, alpha0=None, tol=0.1, callback=None, verbose=0,
+                 tolerance_decrease='exponential', max_iter=10):
+        self.alpha0 = alpha0
+        self.tol = tol
+        self.callback = callback
+        self.verbose = verbose
+        self.tolerance_decrease = tolerance_decrease
+        self.max_iter = max_iter
+
+    def fit(self, Xt, yt, Xh, yh, callback=None):
+        lbin = LabelBinarizer()
+        lbin.fit(yt)
+        Yt_multi = lbin.transform(yt)
+        Yh_multi = lbin.transform(yh)
+        sample_weight_train = np.ones(Xt.shape[0])
+        sample_weight_test = np.ones(Xh.shape[0])
+
+        if Yt_multi.shape[1] == 1:
+            Yt_multi = np.hstack([1 - Yt_multi, Yt_multi])
+            Yh_multi = np.hstack([1 - Yh_multi, Yh_multi])
+            print('warning: only two classes detected')
+
+        n_classes = Yt_multi.shape[1]
+        n_features = Xt.shape[1]
+
+        # if not np.all(np.unique(yt) == np.array([-1, 1])):
+        #     raise ValueError
+        x0 = np.zeros(n_features * n_classes)
+
+        # assert x0.size == self.alpha0.size
+
+        def h_func_grad(x, alpha):
+            # x = x.reshape((-1,Yt_multi.shape[1]))
+            return _multinomial_loss_grad(
+                x, Xt, Yt_multi, np.exp(alpha), sample_weight_train)[:2]
+
+        def h_hessian(x, alpha):
+            # x = x.reshape((-1,Yt_multi.shape[1]))
+            return _multinomial_grad_hess(
+                x, Xt, Yt_multi, np.exp(alpha), sample_weight_train)[1]
+
+        def g_func_grad(x, alpha):
+            # x = x.reshape((-1,Yt_multi.shape[1]))
+            return _multinomial_loss_grad(
+                x, Xh, Yh_multi, np.zeros(alpha.size),
+                sample_weight_test)[:2]
+
+        def h_crossed(x, alpha):
+            # return x.reshape((n_classes, -1)) * alpha
+            # x = x.reshape((-1,Yt_multi.shape[1]))
+            tmp = np.exp(alpha) * x
+            return sparse.dia_matrix(
+                (tmp, 0),
+                shape=(n_features * n_classes, n_features * n_classes))
+
+        opt = hoag_lbfgs(
+            h_func_grad, h_hessian, h_crossed, g_func_grad, x0,
+            callback=callback,
+            tolerance_decrease=self.tolerance_decrease,
+            lambda0=self.alpha0, maxiter=self.max_iter,
+            verbose=self.verbose, only_fit=True)
+
+        self.coef_ = opt[0]
+        self.alpha_ = opt[1]
+        return self
+
+    def decision_function(self, X):
+        return X.dot(self.coef_)
+
+    def predict(self, X):
+        return np.sign(self.decision_function(X))
+
 ### The following is adapted from scikit-learn
 
 L2_REG = 0
@@ -138,7 +215,7 @@ def _multinomial_loss(w, X, Y, alpha, sample_weight):
     p += intercept
     p -= logsumexp(p, axis=1)[:, np.newaxis]
     loss = -(sample_weight * Y * p).sum()
-    loss += 0.5 * ((alpha + L2_REG) * w * w).sum()
+    loss += 0.5 * (alpha * w * w).sum()
     p = np.exp(p, p)
     return loss, p, w
 
@@ -190,7 +267,7 @@ def _multinomial_loss_grad(w, X, Y, alpha, sample_weight):
     sample_weight = sample_weight[:, np.newaxis]
     diff = sample_weight * (p - Y)
     grad[:, :n_features] = safe_sparse_dot(diff.T, X)
-    grad[:, :n_features] += (alpha + L2_REG) * w
+    grad[:, :n_features] += alpha * w
     if fit_intercept:
         grad[:, -1] = diff.sum(axis=0)
     return loss, grad.ravel(), p
@@ -263,8 +340,9 @@ def _multinomial_grad_hess(w, X, Y, alpha, sample_weight):
         r_yhat *= sample_weight
         hessProd = np.zeros((n_classes, n_features + bool(fit_intercept)))
         hessProd[:, :n_features] = safe_sparse_dot(r_yhat.T, X)
-        hessProd[:, :n_features] += v * (alpha + L2_REG)
+        hessProd[:, :n_features] += v * alpha
         if fit_intercept:
+            raise ValueError
             hessProd[:, -1] = r_yhat.sum(axis=0)
         return hessProd.ravel()
 
